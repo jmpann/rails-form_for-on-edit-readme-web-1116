36cb59192d5cdba6b948fd0a7576a42594218623
Josh Pann <josh.pann@example.com>
2016-12-31 15:18:14 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :show, :new, :create, :edit]
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  ###knows to match patch requests to the Update action
 end
